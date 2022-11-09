4b1f605e5e3cb98f62a7dcd1c8031d4d846b19c8
GamzeKural <94907870+GamzeKural@users.noreply.github.com>
2022-11-09 11:35:10 +0300

    Add files via upload

---
+++ GamzeNurKural-Week2-Homework/Owner.API/Controllers/OwnerController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Owner.API.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Owner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOwners()
+        {
+            var ownerDatas = new OwnerData().GetAllOwners();
+            return Ok(ownerDatas);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOwner(int id)
+        {
+            var ownerData = new OwnerData().GetAllOwners().Where(x => x.Id == id).FirstOrDefault();
+            if (ownerData == null)
+            {
+                throw new BadHttpRequestException("User not found.");
+                //return NotFound("User not found.");
+            }
+            else
+            {
+                return Ok(ownerData);
+            }
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        public IActionResult CreateOwner(Model.Owner owner)
+        {
+            if (owner == null)
+            {
+                throw new BadHttpRequestException("No data to add.");
+                //return BadRequest();
+            }
+            else
+            {
+                var ownerData = new OwnerData().GetAllOwners();
+                if (owner.Description.ToLower().Contains("hack"))
+                {
+                    throw new BadHttpRequestException("You cannot write 'hack' words in the description.");
+                    //return BadRequest("You cannot write 'hack' words in the description.");
+                }
+                else
+                {
+                    ownerData.Add(owner);
+                    return Ok(owner);
+                }
+            }
+
+        }
+
+        [HttpPut("{id:int}")]
+        [Consumes("application/json")]
+        public IActionResult UpdateOwner(int id, Model.Owner owner)
+        {
+            if (id != owner.Id)
+            {
+                throw new BadHttpRequestException("Id's did not match.");
+                //return BadRequest("Id's did not match.");
+            }
+            else if (owner.Description.ToLower().Contains("hack"))
+            {
+                throw new BadHttpRequestException("You cannot write 'hack' words in the description.");
+                //return BadRequest("You cannot write 'hack' words in the description.");
+            }
+            else
+            {
+                var ownerData = new OwnerData().GetAllOwners().Where(x => x.Id == id).FirstOrDefault();
+
+                ownerData.FirstName = owner.FirstName;
+                ownerData.Surname = owner.Surname;
+                ownerData.Date = owner.Date;
+                ownerData.Description = owner.Description;
+                ownerData.Type = owner.Type;
+                return Ok(owner);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOwner(int id)
+        {
+            var ownerList = new OwnerData().GetAllOwners();
+            var ownerData = new OwnerData().GetAllOwners().Where(x => x.Id == id).FirstOrDefault();
+            if (ownerData == null)
+            {
+                throw new BadHttpRequestException($"Owner with id {id} not found.");
+                //return NotFound($"Owner with id {id} not found.");
+            }
+            else
+            {
+                ownerList.Remove(ownerData);
+                return Ok("Record successfully deleted.");
+            }
+        }
+    }
+}
